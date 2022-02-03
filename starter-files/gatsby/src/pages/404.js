fac05949a10c8daffc7e429bfa9df0dbf68b022d
Erkan <Bezerye@gmail.com>
2022-02-03 17:55:53 +0300

    04

---
+++ starter-files/gatsby/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Four04Page() {
+  return (
+    <div>
+      <p>Woopsie Doopsie No Page For YOU!!!11</p>
+    </div>
+  );
+}
---
+++ starter-files/gatsby/src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SlicemastersPage() {
+  return (
+    <div>
+      <p>Hey I'm Slicemasters Page</p>
+    </div>
+  );
+}
