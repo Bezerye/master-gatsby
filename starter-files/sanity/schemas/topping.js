83c06a0f33c33d59e5fbad450c1c8fc4f038850d
Erkan <Bezerye@gmail.com>
2022-02-08 22:08:44 +0300

    module8

---
+++ starter-files/sanity/schemas/topping.js
@@ -0,0 +1,36 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // Computer name
+  name: 'topping',
+  // Visible title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'Name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is this a vegetarian topping',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ vegetarian, name }) => ({
+      title: `${name} ${vegetarian ? 'V' : ''}`,
+    }),
+  },
+};
